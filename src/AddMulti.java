7ec41ef61ec4ca478c9521ac7668d72616d5aef5
darekar <darekar@192.168.1.7>
2020-04-19 16:54:48 +0530

    commit

---
+++ src/AddMulti.java
@@ -0,0 +1,20 @@
+
+public class AddMulti {
+	public static int a=4;
+	public static int b=4;
+	int c;
+	int d;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int c = a+b;
+		System.out.println("---------------Addition of two numbers----------------------");
+		System.out.println("--------where,a=4 & b=4(given)----------");
+		System.out.println("RESULT IS:"+c);
+		
+		int d = a*b;
+		System.out.println("---------------Multiplication of two numbers----------------------");
+		System.out.println("--------where,a=4 & b=4(given)----------");
+		System.out.println("RESULT IS:"+d);
+
+	}
+}
---
+++ src/Multi.java
@@ -0,0 +1,15 @@
+
+public class Multi {
+	public static int a=4;
+	public static int b=4;
+	int c;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int c = a*b;
+System.out.println("---------------Multiplication of two numbers----------------------");
+System.out.println("--------where,a=4 & b=4(given)----------");
+System.out.println("RESULT IS:"+c);
+
+	}
+}
